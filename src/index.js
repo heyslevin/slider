616c3b0b3333b1f6bed62de0d0b76a3368728c07
memellaguno <memellaguno@gmail.com>
2020-10-23 08:35:58 -0500

    Modules js set up

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import randomColor from "./randomColor.js";
+import dom from "./dom.js";
+import controller from "./controller.js";
+
+//Selectors
+var slides = document.querySelectorAll(".sliderImage");
+var slideArray = [...slides];
+
+alert("works");
